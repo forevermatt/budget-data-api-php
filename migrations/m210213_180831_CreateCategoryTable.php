b2e138fabdfeec2ccd95d8c242990a59b596cdec
forevermatt <forevermatt@users.noreply.github.com>
2021-02-13 13:11:45 -0500

    Add migration for category table

---
+++ migrations/m210213_180831_CreateCategoryTable.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m210213_180831_CreateCategoryTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'uuid' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
