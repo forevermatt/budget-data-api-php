21e2ea787d747ac6281c59ff6ad1ebd4abc69405
forevermatt <forevermatt@users.noreply.github.com>
2021-03-18 18:57:48 -0400

    Map `/accounts` to viewing the list of Accounts

--- config/web.php
+++ config/web.php
@@ -49,6 +49,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                 'accounts/' => 'account/index',
             ],
         ],
     ],
